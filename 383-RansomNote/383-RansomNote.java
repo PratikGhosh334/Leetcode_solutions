83e1fa99d1e709c58a48513e362194df587c55ca
PratikGhosh <124373051+PratikGhosh334@users.noreply.github.com>
2025-05-24 01:50:04 +0530

    Create 383-RansomNote

---
+++ 383-RansomNote/383-RansomNote.java
@@ -0,0 +1,17 @@
+// Last updated: 5/24/2025, 1:50:03 AM
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+		if (ransomNote.length() > magazine.length()) return false;
+        int[] alphabets_counter = new int[26];
+        
+        for (char c : magazine.toCharArray())
+            alphabets_counter[c-'a']++;
+
+        for (char c : ransomNote.toCharArray()){
+            if (alphabets_counter[c-'a'] == 0) return false;
+            alphabets_counter[c-'a']--;
+        }
+        return true;
+    }
+}
+
